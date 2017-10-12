5d6f9f754527726f58723dba1d656628e423d987
Albert Samseer <albert@samzdesign.nl>
2017-10-12 13:04:03 +0200

    Fixed bug app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('express-cors');
-const config = require('config.json');
+const config = require('./config.json');
 
 //Controllers
 const ReviewsController = require('./controllers/ReviewsController');
